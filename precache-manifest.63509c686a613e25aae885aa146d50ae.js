0121b3b61e68ae7501f4a581c9d2072d620820e7
Michaeldremy <michaeldremy@gmail.com>
2020-07-22 16:59:06 -0700

    Updates

---
+++ precache-manifest.63509c686a613e25aae885aa146d50ae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7c30a2348b4101857861f567b350cc",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "faf8a3d107421c667720",
+    "url": "/Portfolio-Website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d076c6382fdf70b0b21d",
+    "url": "/Portfolio-Website/static/css/main.61cee470.chunk.css"
+  },
+  {
+    "revision": "faf8a3d107421c667720",
+    "url": "/Portfolio-Website/static/js/2.9ddc4751.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-Website/static/js/2.9ddc4751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d076c6382fdf70b0b21d",
+    "url": "/Portfolio-Website/static/js/main.a0941b78.chunk.js"
+  },
+  {
+    "revision": "81dfee1bcd1cfd101105",
+    "url": "/Portfolio-Website/static/js/runtime-main.8928ca10.js"
+  },
+  {
+    "revision": "fe8e37a534c1a0ebadeb7141c56cc15e",
+    "url": "/Portfolio-Website/static/media/Covid19Photo.fe8e37a5.JPG"
+  },
+  {
+    "revision": "c1055e12807a53bd42b31ad98a57c24b",
+    "url": "/Portfolio-Website/static/media/Michael_Remy.c1055e12.jpg"
+  },
+  {
+    "revision": "037f2f02b9a41c1b09410292ceca001c",
+    "url": "/Portfolio-Website/static/media/SpiderGame.037f2f02.JPG"
+  },
+  {
+    "revision": "72e54d54256ea0a4e8be4ec8a3811b0a",
+    "url": "/Portfolio-Website/static/media/gym_bro.72e54d54.JPG"
+  },
+  {
+    "revision": "b55e0466c15e8c76b662be2fd5ed8fd9",
+    "url": "/Portfolio-Website/static/media/page1resume.b55e0466.JPG"
+  },
+  {
+    "revision": "036539ca1958b8370ba5b2a6585ccf1b",
+    "url": "/Portfolio-Website/static/media/page2resume.036539ca.JPG"
+  }
+]);
