ebf088e45db526b176ac03866e6b0ecc77486ec2
Michaeldremy <michaeldremy@gmail.com>
2020-07-22 16:53:55 -0700

    more changes

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,78 +1,62 @@
 import '../views/styles/ContactForm.css';
+import emailjs from "emailjs-com";
 import React from 'react';
-import { Component } from 'react';
-import axios from 'axios';
+import swal from 'sweetalert';
 
-class ContactForm extends Component {
-    constructor() {
-        super()
-        
-        this.state = {
-            name: '',
-            email: '',
-            subject: '',
-            message: '',
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+export default function ContactUs() {
+
+    function sweetAlertClick() {
+        swal("Message sent!", "Please allow me 24 hours to respond. Have a great day!", "success")
     }
 
-    
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-
-    async handleSubmit(e) {
+    function sendEmail(e) {
         e.preventDefault();
-        const {name, email, subject, message} = this.state;
-
-        const url = 'http:localhost:8000'
-
-        const form = await axios.post(`/api/form`, {
-            name,
-            email,
-            subject,
-            message
-        })
-
-        console.log(form)
+        emailjs
+            .sendForm(
+                "gmail",
+                "michaeltemplate",
+                e.target,
+                "user_5Bf1J7iCd4fehUqEZhouf"
+            )
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            e.target.reset();
     }
     
-    render() {
-        return (
-            <div>
-            <div className="container">
-                <div className="row text-center pt-5 pb-5">
-                    <div className="col-12 pt-5">
-                        <h1 id="FormH1">Contact Me</h1>
-                    </div>
-                    <div className="col-12">
-                        <p id="FormP">→ I'll be happy to answer any questions ←</p> 
-                    </div>
-                    <form onSubmit = {this.handleSubmit}>
-                        <div className="row pt-5 mx-auto">
-                            <div className="col-8 form-group mx-auto">
-                                <input type="text" className="form-control" placeholder="Name" name="name" onChange={this.handleChange}/>
-                            </div>
-                            <div className="col-8 form-group pt-2 mx-auto">
-                                <input type="email" className="form-control" placeholder="Email Address" name="email" onChange={this.handleChange}/>
-                            </div>
-                            <div className="col-8 form-group pt-2 mx-auto">
-                                <input type="text" className="form-control" placeholder="Subject" name="subject" onChange={this.handleChange}/>
-                            </div>
-                            <div className="col-8 form-group pt-2 mx-auto">
-                                <textarea name="" className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message" onChange={this.handleChange}></textarea>
-                            </div>
-                            <div className="col-8 pt-3 mx-auto">
-                                <button type="submit" className="btn btn-primary">Send Message</button>
-                            </div>
-                        </div>
-                    </form>
+    return (
+        <div>
+        <div className="container">
+            <div className="row text-center pt-5 pb-5">
+                <div className="col-12 pt-5">
+                    <h1 id="FormH1">Contact Me</h1>
                 </div>
+                <div className="col-12">
+                    <p id="FormP">→ I'll be happy to answer any questions ←</p> 
+                </div>
+                <form onSubmit = {sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-secondary" value="Send Message" onClick={sweetAlertClick}></input>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-        );
-    };
+    </div>
+    );
 }
-    
-export default ContactForm;
--- client/src/views/Sections.jsx
+++ client/src/views/Sections.jsx
@@ -4,52 +4,83 @@ import Fade from 'react-reveal/Fade';
 import LightSpeed from 'react-reveal/LightSpeed';
 import Michael_Remy from '../imgs/Michael_Remy.jpg';
 import Covid19Photo from '../imgs/Covid19Photo.JPG';
-import ContactForm from '../components/ContactForm.js';
+import SpiderGame from '../imgs/SpiderGame.JPG';
+import ContactUs from '../components/ContactForm.js';
+import page1resume from '../imgs/page1resume.JPG';
+import page2resume from '../imgs/page2resume.JPG';
+import gym_bro from '../imgs/gym_bro.JPG';
 
 const Sections = () => {
 
     return (
         <div>
             <div id="NavBar" className="row text-center">
-                <div className="col-4">
+                <div className="col-3">
                     <a href="#sec1">Michael Remy</a>
                 </div>
-                <div className="col-4">
+                <div className="col-3">
                     <a href="#sec2">Projects</a>
                 </div>
-                <div className="col-4">
+                <div className="col-3">
+                    <a href="#sec2-5">Resume</a>
+                </div>
+                <div className="col-3">
                     <a href="#sec3">Contact</a>
                 </div>
             </div>
             <section id="sec1">
                 <div className="row text-center pt-5">
                     <div className="col-12 pt-5">
-                        <img id="sec1Photo" src={Michael_Remy} alt=""/>
-                        <Fade top cascade> <h1>Michael Remy</h1></Fade>
-                        <LightSpeed left cascade> <h2>Full Stack Developer</h2> </LightSpeed>
+                        <img id="sec1Photo" src={Michael_Remy} alt="" className=""/>
+                        <Fade top cascade> <h1 className="pt-4">Michael Remy</h1></Fade>
+                        <LightSpeed left cascade> <h2 className="pt-4">Full Stack Developer</h2> </LightSpeed>
                     </div>
                 </div>
             </section>
             <section id="sec2">
-                <div  id="section2" className="row text-center pt-5">
+                <div className="row pt-5">
+                    <div className="col-md-12">
+                        <p className="pt-3">Click on the photos to go to the deployed websites!</p>
+                    </div>
+                </div>
+                <div id="section2" className="row text-center">
                     <div className="col-md-4">
                         <h3>Gym Bro</h3>
                         <p>Intuitive gym progress tracking website</p>
-                        <img src="" alt=""/>
+                        <a href="http://michaeldremy.pythonanywhere.com/" target="_blank" rel="noopener noreferrer"><img id="gymbroImg" src={gym_bro} alt=""/></a>
+                        <p><a href="https://github.com/Michaeldremy/Gym_Bro_Python_Project" target="_blank" rel="noopener noreferrer"><button className="mt-4 btn btn-secondary">See Code</button></a></p>
                     </div>
                     <div className="col-md-4">
                         <h3>Spider Queen Vol 1.</h3>
                         <p>Game created within Unity</p>
+                        <a href="https://connect.unity.com/mg/other/untitled-6364" target="_blank" rel="noopener noreferrer"><img id="SpiderImg" src={SpiderGame} alt=""/></a>
+                        <p><a href="https://github.com/Michaeldremy/Spider-Queen-Vol-1-C-Sharp-Project" target="_blank" rel="noopener noreferrer"><button className="mt-4 btn btn-secondary">See Code</button></a></p>
                     </div>
                     <div className="col-md-4">
                         <h3>Covid Tracker</h3>
                         <p>Covid-19 Data Visualization website</p>
-                        <img id="CovidImg" src={Covid19Photo} alt=""/>
+                        <a href="https://michaeldremy.github.io/COVID-19-Tracker-Application/" target="_blank" rel="noopener noreferrer"><img id="CovidImg" src={Covid19Photo} alt=""/></a>
+                        <p><a href="https://github.com/Michaeldremy/COVID-19-Tracker-Application/tree/master" target="_blank" rel="noopener noreferrer"><button className="mt-4 btn btn-secondary">See Code</button></a></p>
+                    </div>
+                </div>
+                <div id="section2" className="row text-center pt-5">
+                    <div className="col-md-12">
+                        <h3>Tweet Me</h3>
+                        <p>Twitter Inspired Application</p>
+                        <p className="pt-4">Coming Soon...</p>
+                        <a href="www.google.com" target="_blank" rel="noopener noreferrer"><img src="" alt=""/></a>
+                        <p><a href="https://github.com/Michaeldremy/tweetme" target="_blank" rel="noopener noreferrer"><button className="btn btn-secondary">See Code</button></a></p>
                     </div>
                 </div>
             </section>
+            <section id="sec2-5">
+                <div className="row text-center pt-5">
+                    <div className="col-6 pt-3"><img src={page1resume} alt="" id="section2-5"/></div>
+                    <div className="col-6 pt-4"><img src={page2resume} alt="" id="section2-5"/></div>
+                </div>
+            </section>
             <section id="sec3">
-                <ContactForm />
+                <ContactUs/>
             </section>
         </div>
     );
