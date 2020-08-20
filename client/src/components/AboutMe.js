a82196f7b4e04cef34baccd289e8077646234726
Michaeldremy <michaeldremy@gmail.com>
2020-08-20 12:10:35 -0700

    Changes

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -1,12 +1,14 @@
 import React, {useEffect} from 'react';
 import '../views/styles/AboutMe.css';
-import csharp from '../imgs/csharp.png'
-import css from '../imgs/css.png'
-import html from '../imgs/html.png'
-import reactimg from '../imgs/reactimg.png'
-import redux from '../imgs/redux.png'
-import python from '../imgs/python.png'
-import django from '../imgs/django.png'
+import arrow_image from '../imgs/arrow_img.png';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import CopyrightIcon from '@material-ui/icons/Copyright';
+import gym_bro from '../imgs/gym_bro.JPG';
+import Covid19Photo from '../imgs/Covid19Photo.JPG'
+import crepuscolo from '../imgs/crepuscolo.JPG'
+import SpiderGame from '../imgs/SpiderGame.JPG';
 
 function AboutMe() {
 
@@ -26,15 +28,183 @@ function AboutMe() {
 
     return (
         <div>
-            <section id="SectionAboutMe">
-                <img src={reactimg} alt="" data-speed="3" className="layer"/>
-                <img src={django} alt="" data-speed="2" className="layer"/>
-                <img src={redux} alt="" data-speed="2" className="layer"/>
-                <img src={html} alt="" data-speed="-3" className="layer"/>
-                <img src={css} alt="" data-speed="5" className="layer"/>
-                <img src={python} alt="" data-speed="-3" className="layer"/>
-                <img src={csharp} alt="" data-speed="-4" className="layer"/>
-                <h3 className="" data-speed="1">Michael Remy</h3>
+            <div className="container-fluid">
+            <section className="intro-container">
+                <div className="row text-center">
+                    <div className="col">
+                        <div className="wrapper">
+                            <div className="box-area">
+                                <div className="box-1"></div>
+                                <div className="box-2"></div>
+                                <div className="box-3"></div>
+                                <div className="box-4"></div>
+                                <div className="box-5"></div>
+                                <div className="box-6"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-12">
+                        <h1 className="full-name">Michael Remy</h1>
+                    </div>
+                    <div className="col-12 col-sm-12">
+                        <p className="sub-name pt-4">Full-stack web developer</p>
+                    </div>
+                </div>
+                <div className="row learn-more-container">
+                    <div className="col-12 col-sm-12">
+                        <a href="#about-me-section-scroll"><p className="learn-more">Learn More</p></a>
+                    </div>
+                    <div className="col-12 col-sm-12">
+                        <a href="#about-me-section-scroll"><img src={arrow_image} alt=""/></a>
+                    </div>
+                </div>
+            </section>
+            </div>
+            {/* main page ends */}
+            <section className="about-me-container">
+                <div className="row text-center">
+                    <div id="about-me-section-scroll"></div>
+                    <div className="col-12">
+                        <h1 className="about-me-header">About Me</h1>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="row text-center">
+                    <div className="col-12" id="info-text-container">
+                        <h5>Hello! I'm Michael a software developer with interest in music, outdoors and reading books.</h5>
+                        <h5>I love solving challenging problems that require exceptional critical thinking, and I love to teach others.</h5>
+                        <h5>I have huge ambitions and dreams and I am always figuring out the most optimal way to accomplish them.</h5>
+                        <h5>I am skilled in three full stacks: Python/Django, MERN, C#/ASP.NET.</h5>
+                        <div className="line-bottom"></div>
+                    </div>
+                </div>
+                <div className="row text-center social-media-container">
+                    <a href="https://github.com/Michaeldremy" target="_blank" rel="noopener noreferrer"><GitHubIcon style={{fontSize: '57px', marginTop: '8px', color: 'black'}}></GitHubIcon></a>
+                    <a href="https://www.linkedin.com/in/michaeldremy/" target="_blank" rel="noopener noreferrer"><LinkedInIcon style={{fontSize: '75px', color: '#005b8e', marginRight: '3rem', marginLeft: '3rem'}}></LinkedInIcon></a>
+                    <a href="mailto:michaeldremy@gmail.com"><MailOutlineIcon style={{fontSize: '75px', color: '#DEF2F1', opacity: '.85'}}></MailOutlineIcon></a>
+                </div>
+                <div id="scroll-container">
+                    <div>
+                        <a href="#" className="scroll-down"></a>
+                    </div>
+                    <div className="mouse">
+                        <span></span>
+                    </div>
+                    <div className="arrow">
+                        <span></span>
+                    </div>
+                </div>
+            </section>
+            {/* about me ends */}
+            <section className="container-fluid project-one-container">
+                <div className="row">
+                    <div className="col-5 gym-bro-image">
+                        <a href="http://michaeldremy.pythonanywhere.com/" target="_blank" rel="noopener noreferrer"><img src={gym_bro} alt=""/></a>
+                    </div>
+                    <div className="col-7 text-center gym-bro-text">
+                        <h1>Gym Bro | Workout stat tracking web app</h1>
+                        <h5>
+                            Workout tracking web app complete with aesthetic landing page, user login/registration 
+                            to allow users to select a daily workout comprising of curated exercise sets, 
+                            and view a graphical representation of their workout stats.
+                        </h5>
+                        <p>
+                            Github: <a href="https://github.com/Michaeldremy/Gym_Bro_Python_Project" target="_blank" rel="noopener noreferrer"><GitHubIcon style={{fontSize: '23px', color: '#feffff', marginBottom: '6px'}}></GitHubIcon></a> | <a href="http://michaeldremy.pythonanywhere.com/" target="_blank" rel="noopener noreferrer">View Live Website</a> | <a href="#">View Project Demo</a>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            {/* project one ends */}
+            <section className="container-fluid project-two-container">
+                <div className="row">
+                    <div className="col-7 text-center covid-image-text">
+                        <h1>Covid-19 Tracker | Real-time Case tracking</h1>
+                        <h5>
+                            Completely responsive data visualization wep application that has daily updates to track infected, recovered and deaths over 100+ countries. 
+                        </h5>
+                        <p>
+                            Github: <a href="https://github.com/Michaeldremy/COVID-19-Tracker-Application" target="_blank" rel="noopener noreferrer"><GitHubIcon style={{fontSize: '23px', color: '#feffff', marginBottom: '6px'}}></GitHubIcon></a> | <a href="https://michaeldremy.github.io/COVID-19-Tracker-Application/" target="_blank" rel="noopener noreferrer">View Live Website</a> | <a href="#">View Project Demo</a>
+                        </p>
+                    </div>
+                    <div className="col-5 covid-image">
+                        <a href="https://michaeldremy.github.io/COVID-19-Tracker-Application/" target="_blank" rel="noopener noreferrer"><img src={Covid19Photo} alt=""/></a>
+                    </div>
+                </div>
+            </section>
+            {/* project two ends */}
+            <section className="container-fluid project-three-container">
+                <div className="row">
+                    <div className="col-5 crepuscolo-image">
+                        <a href="https://michaeldremy.github.io/The-Crepuscolo/index.html" target="_blank" rel="noopener noreferrer"><img src={crepuscolo} alt=""/></a>
+                    </div>
+                    <div className="col-7 text-center crepuscolo-text">
+                        <h1>The Crepuscolo | Restaurant Landing Page</h1>
+                        <h5>
+                            Italian restaurant landing page that is completely responsive 
+                            and has beautiful animations creating an appetizing user experience.
+                        </h5>
+                        <p>
+                            Github: <a href="https://github.com/Michaeldremy/The-Crepuscolo" target="_blank" rel="noopener noreferrer"><GitHubIcon style={{fontSize: '23px', color: '#feffff', marginBottom: '6px'}}></GitHubIcon></a> | <a href="https://michaeldremy.github.io/The-Crepuscolo/" target="_blank" rel="noopener noreferrer">View Live Website</a> | <a href="#">View Project Demo</a>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            {/* project three ends */}
+            <section className="container-fluid project-four-container">
+                <div className="row">
+                    <div className="col-7 text-center spider-image-text">
+                        <h1>Spider Queen | FPS Game</h1>
+                        <h5>
+                            First-person shooter game created with C# and Unity. Goal of the game is to complete objectives and defeat the spider queen by solving puzzles.
+                        </h5>
+                        <p>
+                            Github: <a href="https://github.com/Michaeldremy/Spider-Queen-Vol-1-C-Sharp-Project" target="_blank" rel="noopener noreferrer"><GitHubIcon style={{fontSize: '23px', color: '#feffff', marginBottom: '6px'}}></GitHubIcon></a> | <a href="https://connect.unity.com/mg/other/untitled-6364" target="_blank" rel="noopener noreferrer">View Live Website</a> | <a href="#">View Project Demo</a>
+                        </p>
+                    </div>
+                    <div className="col-5 spider-image">
+                        <a href="https://connect.unity.com/mg/other/untitled-6364" target="_blank" rel="noopener noreferrer"><img src={SpiderGame} alt=""/></a>
+                    </div>
+                </div>
+            </section>
+            {/* project four ends */}
+            <section className="container-fluid resume-container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Resume</h1>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="row resume-links">
+                    <div className="col-12 pb-4">
+                        <a href="../components/michael-remy-resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>
+                    </div>
+                    <div className="col-12">
+                        <a href="../components/michael-remy-resume.pdf" download>Download my resume</a>
+                    </div>
+                </div>
+            </section>
+            {/* resume ends */}
+            <section className="container-fluid contact-footer-container">
+                <div className="row contact-container">
+                    <div className="col-6">
+                        <h1>Contact</h1>
+                        <p>For more information, please contact me by e-mail or LinkedIn Message. Thank you!</p>
+                    </div>
+                    <div className="col-6">
+                        <button className="email-button">email</button>
+                        <button className="linkedin-button">LinkedIn</button>
+                    </div>
+                </div>
+                <div className="row footer-container">
+                    <div className="col-12">
+                    <a href="https://github.com/Michaeldremy" target="_blank" rel="noopener noreferrer"><GitHubIcon style={{fontSize: '30px', marginTop: '1px', color: 'white', opacity: '.6'}}></GitHubIcon></a>
+                    <a href="https://www.linkedin.com/in/michaeldremy/" target="_blank" rel="noopener noreferrer"><LinkedInIcon style={{fontSize: '34px', color: 'white', opacity: '.6', marginRight: '2rem', marginLeft: '2rem'}}></LinkedInIcon></a>
+                    <a href="mailto:michaeldremy@gmail.com"><MailOutlineIcon style={{fontSize: '33px', color: 'white', opacity: '.7', marginBottom: '2px'}}></MailOutlineIcon></a>
+                    </div>
+                    <div className="col-12">
+                        <h1><CopyrightIcon style={{fontSize: '17px', marginRight: '10px', marginBottom: '2px'}}></CopyrightIcon>Michael Remy</h1>
+                    </div>
+                </div>
             </section>
         </div>
     )
--- client/src/views/Sections.jsx
+++ client/src/views/Sections.jsx
@@ -1,13 +1,8 @@
 import React, { useEffect } from 'react';
 import './styles/SectionsStyle.css';
-import Covid19Photo from '../imgs/Covid19Photo.JPG'
-import SpiderGame from '../imgs/SpiderGame.JPG';
-import ContactUs from '../components/ContactForm.js';
 import Aos from 'aos';
 import "aos/dist/aos.css";
-import gym_bro from '../imgs/gym_bro.JPG';
 import AboutMe from '../components/AboutMe';
-import githubIcon from '../imgs/githubIcon.png'
 
 const Sections = () => {
 
@@ -17,7 +12,7 @@ const Sections = () => {
 
     return (
         <div>
-            <div id="NavBar" className="row text-center">
+            {/* <div id="NavBar" className="row text-center">
                 <div className="col-4">
                     <a href="#sec1">Michael Remy</a>
                 </div>
@@ -27,70 +22,13 @@ const Sections = () => {
                 <div className="col-4">
                     <a href="#sec3">Contact</a>
                 </div>
-            </div>
+            </div> */}
             <section id="sec1">
                 <AboutMe />
             </section>
-            <section id="sec2">
-                <div className="container">
-                    <div className="col-12">
-                        <h5 className="h5ProjectHeader" data-aos="fade-down">PROJECTS</h5>
-                    </div>
-                </div>
-                <div id="section2" className="row text-center">
-                    <div className="col-md-6" style={{ paddingTop: "5%" }}>
-                        <a href="http://michaeldremy.pythonanywhere.com/" target="_blank" rel="noopener noreferrer"><h5 className="h5GymBro" data-aos="fade-right">GYM BRO</h5></a>
-                        <div className="projectBoxGymBro" data-aos="fade-right">
-                            <h6>Full-stack web development using Python and Django to allow users to select a daily workout comprising of curated exercise sets,
-                                keep track of their accomplishments and view a graphical representation of their workout stats.</h6>
-                        </div>
-                        <div>
-                            <h3 className="h3GymBro" data-aos="fade-right">Python | Django | ChartIt</h3>
-                        </div>
-                        <div data-aos="fade-right">
-                            <a href="https://github.com/Michaeldremy/Gym_Bro_Python_Project" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="" id="GymBroGitHubIconBorder" /></a>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <a href="http://michaeldremy.pythonanywhere.com/" target="_blank" rel="noopener noreferrer"><img src={gym_bro} alt="" id="gymbroImg" data-aos="fade-left" /></a>
-                    </div>
-                    <div className="col-md-6">
-                        <a href="https://michaeldremy.github.io/COVID-19-Tracker-Application/" target="_blank" rel="noopener noreferrer"><img src={Covid19Photo} alt="" id="covidImg" data-aos="fade-right" /></a>
-                    </div>
-                    <div className="col-md-6" style={{ paddingTop: "5%" }}>
-                        <a href="https://michaeldremy.github.io/COVID-19-Tracker-Application/" target="_blank" rel="noopener noreferrer"><h5 className="h5Covid" data-aos="fade-left">COVID-19 TRACKER</h5></a>
-                        <div className="projectBoxCovid" data-aos="fade-left">
-                            <h6>Full-stack web development using MERN (MongoDB, Express, React, Node.js)
-                                    to create a completely responsive data visualization website to track COVID-19 information.</h6>
-                        </div>
-                        <div>
-                            <h3 className="h3Covid" data-aos="fade-left">React | MongoDB | Express | Node.js</h3>
-                        </div>
-                        <div data-aos="fade-left">
-                            <a href="https://github.com/Michaeldremy/COVID-19-Tracker-Application/tree/master" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="" id="CovidGitHubIconBorder" /></a>
-                        </div>
-                    </div>
-                    <div className="col-md-6" style={{ paddingTop: "5%" }}>
-                        <a href="https://connect.unity.com/mg/other/untitled-6364" target="_blank" rel="noopener noreferrer"><h5 className="h5Spider" data-aos="fade-right">SPIDER QUEEN</h5></a>
-                        <div className="projectBoxSpider" data-aos="fade-right">
-                            <h6>Game created within Unity using C# object orientated programming to find weapons, ammo, navigate through rooms of animated zombies 
-                                and giant spiders, and attempt to find and destroy the Spider Queen in this first person shooter game.</h6>
-                        </div>
-                        <div>
-                            <h3 className="h3Spider" data-aos="fade-right">C# | JavaScript | Unity</h3>
-                        </div>
-                        <div data-aos="fade-right">
-                            <a href="https://github.com/Michaeldremy/Spider-Queen-Vol-1-C-Sharp-Project" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="" id="SpiderGitHubIconBorder" /></a>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <a href="https://connect.unity.com/mg/other/untitled-6364" target="_blank" rel="noopener noreferrer"><img src={SpiderGame} alt="" id="SpiderImg" data-aos="fade-left" /></a>
-                    </div>
-                </div>
-            </section>
-            <section id="sec3">
+            {/* <section id="sec3">
                 <ContactUs />
-            </section>
+            </section> */}
         </div>
     );
 };
