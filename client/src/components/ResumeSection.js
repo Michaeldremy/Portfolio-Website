89f68403f918ef371b1c237cab7a39d8424f52b1
Michaeldremy <michaeldremy@gmail.com>
2020-10-27 10:38:44 -0700

    updated resume link

--- client/src/components/ResumeSection.js
+++ client/src/components/ResumeSection.js
@@ -7,7 +7,7 @@ const ResumeSection = () => {
   return (
     <div>
       <div className="resume-section-container">
-        <a href="https://github.com/Michaeldremy/Portfolio-Website/blob/master/client/src/components/Michael_Remy_Resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>
+        <a href="https://github.com/Michaeldremy/Portfolio-Website/blob/master/client/src/components/Official_Michael_Remy_Resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>
       </div>
     </div>
   )
